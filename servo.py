6c98084e789e21f5d4f74ceca006ec78e02f3cdb
analyzerlabs <fisicomiguel@gmail.com>
2019-06-29 10:06:38 -0500

    prueba rpi plus autoupdate

--- servo.py
+++ servo.py
@@ -4,6 +4,9 @@ import time
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD  
 GPIO.setup(20, GPIO.IN)  # set a port/pin as an input  
 GPIO.setup(10, GPIO.OUT)  # set a port/pin as an input 
+GPIO.setup(2, GPIO.OUT)  # set a port/pin as an input 
+GPIO.setup(3, GPIO.OUT)  # set a port/pin as an input 
+
 m = GPIO.PWM(10,360)
 m.start(0)
 vueltas = 0
@@ -11,8 +14,8 @@ ant = 0
 ant_vueltas = 0
 seconds = time.time()
 while True:
-	#angulo = int(input("Ingrese angulo: "))	
-	
+	#angulo = int(input("Ingrese angulo: "))
+	GPIO.output(2,False)
 	print seconds
 	#while vueltas< 100:
 	for i in range(0,45):
