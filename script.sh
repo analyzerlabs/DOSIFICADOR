ec043f3f24dee8db96e880d2da8175d41ab158bf
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-14 11:42:48 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd /home/pi/DOSIFICADOR
 
-while true;do
-	echo "====================================="
-	echo "============ Actualizando ==========="
-	sudo git pull origin master
-	echo "====================================="
-	echo "========== Ejecuta Software ========="
-	sudo python limaem.py &
-	sleep 14400
-done
+sudo git pull origin master
+echo "===== ejecutando python file ====="
+sudo python limaem.py &
