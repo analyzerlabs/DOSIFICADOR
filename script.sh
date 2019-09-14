4770d9b7d71f06747f6c37c38becb51a93b286a4
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-14 11:06:40 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
-cd C:/Users/Milkito/Documents/Github/DOSIFICADOR
-
-echo "please enter your name:"
-read myname
-echo "ypu entered: $myname"
-
-myvar=1
-
-while [ $myvar -le 10 ]
-do
-	echo $myvar
-	myvar=$(($myvar + 1))	
-	sleep 1
-done
-
-if [ -f ~/limaem.py ]
-then
-	echo "the file exist"
-else 
-	echo "file doesnt exist!!"
-fi
+cd /home/pi/DOSIFICADOR
 
 while true;do
-	echo "pid is $$"
-	echo $$ > /tmp/$(basename $0).pid
-    echo "====================================="
+	echo "====================================="
 	echo "============ Actualizando ==========="
 	git pull origin master
 	echo "====================================="
 	echo "========== Ejecuta Software ========="
 	python limaem.py &
-	sleep 400
+	sleep 14400
 done
