770a939fe300ae23e8927c0bda8edbd27c59383c
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-14 11:40:34 -0500

    Update autoupdater.sh

--- autoupdater.sh
+++ autoupdater.sh
@@ -2,16 +2,14 @@
 cd /home/pi/DOSIFICADOR
 
 while true;do 
-    echo "==========================="
-    echo "======  ACTUALIZANDO  ====="
-    echo "==========================="
-
-    sudo git clone git://github.com/analyzerlabs/DOSIFICADOR.git
-
-    echo "==========================="
-    echo "======  EJECUTANDO..  ====="
-    echo "==========================="
-
+    echo "==============================="
+    echo "========  ACTUALIZANDO  ======="
+    echo "==============================="
+    sudo git reset --hard
+    sudo git pull
+    echo "==============================="
+    echo "========  EJECUTANDO..  ======="
+    echo "==============================="
     sudo /home/pi/DOSIFICADOR/script.sh &
     sleep 14400
 done
