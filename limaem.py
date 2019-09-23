91832268b13f4cef248dc5c4b4fe48d16e26c87e
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-23 17:43:51 -0500

    Update limaem.py

--- limaem.py
+++ limaem.py
@@ -19,7 +19,7 @@ class LimaEM:
 		self.openFiles()
 		self.Serie = self.file_id.readlines()
 	 	self.Serie = int(self.Serie[0])
-		self.v = int(self.volumen[self.Serie-1] / 2.5) - self.error[self.Serie-1] 
+		self.v = int(self.volumen[self.Serie-1] / 2.5) - self.error[self.Serie-1]
 		print ("El volumen a medir sera de : " + str(self.volumen[self.Serie-1]) + " mL")
 		time.sleep(3)
 		GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
@@ -36,20 +36,20 @@ class LimaEM:
 		self.openFiles()
 		GPIO.output(4,GPIO.HIGH)
 		GPIO.output(17,GPIO.HIGH)
-		
+
 	def openFiles(self):
 		self.file_id = open("/home/pi/id.txt","r")
 		self.file_intento = open("/home/pi/intento.txt","a")
 		self.file_dosis = open("/home/pi/dosis.txt","a")
 		self.file_error = open("/home/pi/error.txt","a")
 		self.file_itsalive = open("/home/pi/itsalive.txt","a")
-	
+
 	def closeFiles(self):
 		self.file_intento.close()
 		self.file_dosis.close()
 		self.file_error.close()
 		self.file_itsalive.close()
-		
+
 	def counter(self,pin):
 		self.cont += 1
 		print self.cont
@@ -63,16 +63,16 @@ class LimaEM:
 		for i in range(0,14):
 			time.sleep(0.05)
 			self.m.ChangeDutyCycle(self.min_angle[self.Serie-1]+i)
-		self.m.ChangeDutyCycle(self.min_angle[self.Serie-1]+13)	
+		self.m.ChangeDutyCycle(self.min_angle[self.Serie-1]+13)
 		self.m.stop()
-		
+
 	def measureVolume(self,fecha):
 		self.file_intento.write(fecha + "\t Intento de Dosificacion \n")
 		t0 = time.time()
 		while GPIO.input(20) != 0 :
 			time.sleep(0.0001)
 			t1 = time.time()
-			# el volumen se define aqui 
+			# el volumen se define aqui
 			if(t1 - t0 >= 20):
 				print("\t===== Error : Tiempo de Lectura Excedido , Error Tanque Vacio")
 				t0 = t1
@@ -98,7 +98,7 @@ class LimaEM:
 		self.ant_cont = 0
 		carnes.closeValve()
 		return self.cont
-		
+
 	def blinkLed(self,pin,state):
 		GPIO.output(pin,state)
 
@@ -117,7 +117,7 @@ while(condition):
 	signal = 1 - signal
 	carnes.blinkLed(4,signal)
 	time.sleep(1)
-	fecha = time.strftime("%Y-%m-%d %H:%M:%S") 
+	fecha = time.strftime("%Y-%m-%d %H:%M:%S")
 	hora  = time.strftime("%H")
 	minuto = time.strftime("%M")
 	if(int(hora)%4 == 0 and int(minuto) == 10):
@@ -128,22 +128,21 @@ if(int(hora)%4 == 2 and int(minuto) == 10):
 	carnes.openFiles()
 	print("\t--------------------------------- ")
 	print("\t===== EJECUTANDO NUEVA DOSIS ==== ")
-	print("\t===== FECHA : " + fecha) 
+	print("\t===== FECHA : " + fecha)
 	carnes.openValve()
 	vol = carnes.measureVolume(fecha)
 	carnes.file_dosis.write("\t"+ fecha + "\t volumen= "+ str(2.5*(vol+carnes.error[carnes.Serie-1])))
 	carnes.closeFiles()
 	GPIO.setup(10,GPIO.IN)
 	GPIO.cleanup()
-	
+
 if(int(minuto)%5 == 0 and itsaliveFlag == True):
 	print("saving its alive")
 	carnes.openFiles()
 	carnes.file_itsalive.write(fecha + "\t Raspberry Encendida\n")
 	carnes.closeFiles()
 	itsaliveFlag = False
-	
+
 if(int(minuto)%5 == 1 and itsaliveFlag == False):
 	itsaliveFlag = True
 
-curses.endwin()
