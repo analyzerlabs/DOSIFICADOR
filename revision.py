9444d9be62356d47688ea7eca5c546406fe83055
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-23 18:21:29 -0500

    Update revision.py

--- revision.py
+++ revision.py
@@ -4,8 +4,8 @@ import time
 import datetime
 
 def openFiles():
-	file_localRev = open("/home/pi/localRev.txt","w")
-    fecha = time.strftime("%Y-%m-%d %H:%M:%S")
+    file_localRev = open("/home/pi/localRev.txt","w")
+    fecha = now.strftime("%m/%d/%Y, %H:%M:%S")
     file_localRev.write(fecha)
     print "Inicio de nueva revision "
     print (fecha)
