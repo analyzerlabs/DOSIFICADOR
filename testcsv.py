329c60d1dfc9aa8b90db4c0ee06ea22394fa54b6
analyzerlabs <miguelquispecastro@uni.pe>
2019-09-24 12:47:10 -0500

    Update testcsv.py

--- testcsv.py
+++ testcsv.py
@@ -1,7 +1,8 @@
 import csv
 
 with open('./data.csv', 'r') as _filehandler:
-    csv_file_reader = csv.DictReader(_filehandler)
+    csv_file_reader = csv.DictReader(_filehandler,delimiter=';',quotechar='|')
+
     for row in csv_file_reader:
         # Do something here
         print(row['id'])
