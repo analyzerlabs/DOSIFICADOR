49583e169ddf9a5f82f127ed318687c467857a1c
analyzerlabs <miguelquispecastro@uni.pe>
2019-10-26 13:59:03 -0500

    Update myemail.py

--- myemail.py
+++ myemail.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -26,6 +27,9 @@ Hola,
 Este email esta siendo enviado como parte de la revision
 Ubiacion : ''' +str(ubicacion[Serie]) +'''\n Sector: ''' + str(seccion[Serie]) + "\n Thank you"
 
+files = "/home/pi"
+filenames = [os.path.join(files, f) for f in os.listdir(files)]
+
 #The mail addresses and password
 sender_address = 'dosificadorlimaem@gmail.com'
 sender_pass = 'LimaEM_dosificador'
@@ -38,15 +42,47 @@ message['Subject'] = 'Revision y Log Equipo ubicado en ' + str(ubicacion[Serie])
 #The subject line
 #The body and the attachments for the mail
 message.attach(MIMEText(mail_content, 'plain'))
-attach_file_name = '/home/pi/dosis.txt'
-attach_file = open(attach_file_name, 'rb') # Open the file as binary mode
-payload = MIMEBase('application', 'octate-stream')
-payload.set_payload((attach_file).read())
-encoders.encode_base64(payload) #encode the attachment
-#add payload header with filename
-payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
-message.attach(payload)
-#Create SMTP session for sending the mail
+
+#attach_file_name = '/home/pi/dosis.txt'
+#attach_file = open(attach_file_name, 'rb') # Open the file as binary mode
+
+# adjunto numero 1 = lastrev.txt
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(open("/home/pi/lastRev.txt", 'rb').read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="/home/pi/lastRev.txt"')
+message.attach(part)#Create SMTP session for sending the mail
+
+# adjunto numero 2 = dosis.txt
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(open("/home/pi/dosis.txt", 'rb').read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="/home/pi/dosis.txt"')
+message.attach(part)#Create SMTP session for sending the mail
+
+# adjunto numero 3 = error.txt
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(open("/home/pi/error.txt", 'rb').read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="/home/pi/error.txt"')
+message.attach(part)#Create SMTP session for sending the mail
+
+
+# adjunto numero 4 = itsalive.txt
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(open("/home/pi/itsalive.txt", 'rb').read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="/home/pi/itsalive.txt"')
+message.attach(part)#Create SMTP session for sending the mail
+
+
+# adjunto numero 5 = intento.txt
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(open("/home/pi/intento.txt", 'rb').read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="/home/pi/intento.txt"')
+message.attach(part)#Create SMTP session for sending the mail
+
 session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
 session.starttls() #enable security
 session.login(sender_address, sender_pass) #login with mail_id and password
