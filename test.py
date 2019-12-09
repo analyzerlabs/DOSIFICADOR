df98ee6d2979e88d79c72b31e90440dd166168c4
MiguelQuispe <51973145+analyzerlabs@users.noreply.github.com>
2019-12-08 19:28:03 -0500

    updates

--- test.py
+++ test.py
@@ -2,31 +2,32 @@ file_intento = open("intento.txt","a")
 file_error = open("error.txt","a")
 
 
-
 Dia  = 15
 Mes  = 10
 Ano  = 19
-hora   = 3
+hora= 3
 minuto  = 10
 segundo = 10
-
+aux="0"
 for j in range (0,30):
+    hora= 3
+    aux="0"
     Dia = Dia + 1
     if(Dia == 31):
         Mes = Mes +1 
         Dia = 1
         
     for i in range (0,6):
+        fecha = str(Dia) + "/"+str(Mes)+"/"+str(Ano) + " " +aux+ str(hora)+ ":10:00"
+        file_intento.write(fecha + "\t Intento de Dosificacion\n")    
         hora = hora + 4
         if(hora > 24):
             hora = hora - 24
-            H= str(hora)
-        elif(hora<10):
-            H = "0"+str(hora)
+            break
+        if(hora > 10):
+            aux=""
     
-         fecha = str(Dia) + "/"+str(Mes)+"/"+str(Ano) + " " + H+ ":10:00"
         
-        file_intento.write(fecha + "\t Intento de Dosificacion\n")    
         #file_error.write(fecha + "\t Error : Tiempo de Lectura Excedido , Error Tanque Vacio\n")
 
 file_intento.close()
